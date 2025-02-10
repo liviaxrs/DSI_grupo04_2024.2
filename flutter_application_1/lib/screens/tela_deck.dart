37111680186c6ac991f67ffc010a447f59f39394
maltezzzz <victorleitemalta2@gmail.com>
2025-02-10 00:13:14 -0300

    Sincronização dos flashcards e decks com banco de dados, acesso aos decks através de chave e edição do nome dos decks

--- flutter_application_1/lib/screens/tela_deck.dart
+++ flutter_application_1/lib/screens/tela_deck.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'tela_addcard.dart';
 
 class DeckScreen extends StatefulWidget {
+  final String deckId;
   final String deckName;
 
-  const DeckScreen({super.key, required this.deckName});
+  const DeckScreen({super.key, required this.deckId, required this.deckName});
 
   @override
   _DeckScreenState createState() => _DeckScreenState();
@@ -13,44 +15,42 @@ class DeckScreen extends StatefulWidget {
 
 class _DeckScreenState extends State<DeckScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   List<Map<String, String>> cards = [];
+  late String deckName;
 
   @override
   void initState() {
     super.initState();
+    deckName = widget.deckName;
     _loadCards();
   }
 
   void _loadCards() async {
-    QuerySnapshot snapshot = await _firestore
+    String? userId = _auth.currentUser?.uid;
+    if (userId == null) return;
+
+    QuerySnapshot cardsSnapshot = await _firestore
         .collection('decks')
-        .where('name', isEqualTo: widget.deckName)
+        .doc(widget.deckId)
+        .collection('cards')
         .get();
 
-    if (snapshot.docs.isNotEmpty) {
-      String deckId = snapshot.docs.first.id;
-      QuerySnapshot cardsSnapshot = await _firestore
-          .collection('decks')
-          .doc(deckId)
-          .collection('cards')
-          .get();
-
-      setState(() {
-        cards = cardsSnapshot.docs
-            .map((doc) => {
-                  'text': doc['text'] as String,
-                  'answer': doc['answer'] as String,
-                })
-            .toList();
-      });
-    }
+    setState(() {
+      cards = cardsSnapshot.docs
+          .map((doc) => {
+                'text': doc['text'] as String,
+                'answer': doc['answer'] as String,
+              })
+          .toList();
+    });
   }
 
   void _navigateToAddCard() async {
     bool? cardAdded = await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => addCardScreen(deckName: widget.deckName),
+        builder: (context) => AddCardScreen(deckId: widget.deckId),
       ),
     );
 
@@ -59,137 +59,187 @@ class _DeckScreenState extends State<DeckScreen> {
     }
   }
 
+  void _editDeckName() async {
+    TextEditingController _nameController = TextEditingController(text: deckName);
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Editar Nome do Deck"),
+          content: TextField(
+            controller: _nameController,
+            decoration: const InputDecoration(labelText: "Novo nome"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancelar"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                String newName = _nameController.text.trim();
+                if (newName.isNotEmpty) {
+                  await _firestore.collection('decks').doc(widget.deckId).update({'name': newName});
+                  setState(() => deckName = newName);
+                }
+                Navigator.pop(context);
+              },
+              child: const Text("Salvar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.deckName, 
-        style: TextStyle(
-            color: Colors.white, 
-            fontWeight: FontWeight.bold, 
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(context, true); // Retorna true para indicar atualização
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(deckName,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 19, 62, 135),
+          iconTheme: const IconThemeData(color: Colors.white),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context, true); // Garante atualização ao voltar
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.edit, color: Colors.white),
+              onPressed: _editDeckName,
+            ),
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 19, 62, 135),
-        iconTheme: IconThemeData(color: Colors.white), 
-      ),
-      body: Container(
-        color: Colors.grey[200],
-        child: Center(
-          child: cards.isEmpty
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.insert_drive_file,
-                      size: 60,
-                      color: Colors.grey,
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      "Esse deck não possui nenhum card",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, color: Colors.black54),
-                    ),
-                    const SizedBox(height: 16),
-                    ElevatedButton(
-                      onPressed: _navigateToAddCard,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[900],
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 24, vertical: 12),
-                      ),
-                      child: const Text("Adicionar card",
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                  ],
-                )
-              : Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+        body: Container(
+          color: Colors.grey[200],
+          child: Center(
+            child: cards.isEmpty
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const SizedBox(height: 16),
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.grey[300],
-                        child: Text(
-                          cards.length.toString(),
-                          style: const TextStyle(
-                              fontSize: 32, fontWeight: FontWeight.bold),
-                        ),
+                      const Icon(
+                        Icons.insert_drive_file,
+                        size: 60,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Esse deck não possui nenhum card",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16, color: Colors.black54),
                       ),
                       const SizedBox(height: 16),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: _navigateToAddCard,
                         style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue[900],
                           padding: const EdgeInsets.symmetric(
                               horizontal: 24, vertical: 12),
-                          backgroundColor: Colors.blue[900],
                         ),
-                        child: const Text("Começar a estudar!",
+                        child: const Text("Adicionar card",
                             style: TextStyle(color: Colors.white)),
                       ),
-                      const SizedBox(height: 16),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.3),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          padding: const EdgeInsets.all(12),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Flashcards no deck (${cards.length})",
-                                style: const TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(height: 8),
-                              Expanded(
-                                child: ListView.builder(
-                                  itemCount: cards.length,
-                                  itemBuilder: (context, index) {
-                                    return Card(
-                                      elevation: 2,
-                                      margin: const EdgeInsets.symmetric(
-                                          vertical: 6),
-                                      child: ListTile(
-                                        title:
-                                            Text(cards[index]['text'] ?? ""),
-                                        subtitle:
-                                            Text(cards[index]['answer'] ?? ""),
-                                        trailing: IconButton(
-                                          icon: const Icon(Icons.edit),
-                                          onPressed: () {},
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
+                    ],
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 16),
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.grey[300],
+                          child: Text(
+                            cards.length.toString(),
+                            style: const TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                            backgroundColor: Colors.blue[900],
+                          ),
+                          child: const Text("Começar a estudar!",
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                        const SizedBox(height: 16),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Flashcards no deck (${cards.length})",
+                                  style: const TextStyle(
+                                      fontSize: 16, fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(height: 8),
+                                Expanded(
+                                  child: ListView.builder(
+                                    itemCount: cards.length,
+                                    itemBuilder: (context, index) {
+                                      return Card(
+                                        elevation: 2,
+                                        margin: const EdgeInsets.symmetric(
+                                            vertical: 6),
+                                        child: ListTile(
+                                          title: Text(cards[index]['text'] ?? ""),
+                                          subtitle: Text(cards[index]['answer'] ?? ""),
+                                          trailing: IconButton(
+                                            icon: const Icon(Icons.edit),
+                                            onPressed: () {},
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _navigateToAddCard,
-        backgroundColor: const Color.fromARGB(255, 19, 62, 135),
-        child: const Icon(Icons.add, color: Colors.white),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _navigateToAddCard,
+          backgroundColor: const Color.fromARGB(255, 19, 62, 135),
+          child: const Icon(Icons.add, color: Colors.white),
+        ),
       ),
     );
   }
